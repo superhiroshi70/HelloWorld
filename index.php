48823ab3c9f8612692c96d848b7fc5108b656add
hiroshi <superhiroshi70@gmail.com>
2019-02-21 22:52:37 +0900

    kadai 5

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ $array = array(40, 49, 50, 64, 65, 79, 80, 70);
 foreach ($array as $value){
 	if($value < 50){
 		echo "不可 " . $value;
-	}elseif($value <= 50 && 65 < $value){
+	}elseif(50 <= $value && $value < 65){
 		echo "可 " . $value;
-	}elseif($value <= 65 && 80 < $value){
+	}elseif(65 <= $value && $value < 80){
 		echo "良 " . $value;
 	}elseif(80 <= $value){
 		echo "優 " . $value;
